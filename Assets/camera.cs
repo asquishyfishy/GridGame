945203f0dfccb35be9ac6dcfb74c710a1d2d57d0
Tim Sereda <timothy.sereda@trojans.dsu.edu>
2022-06-21 16:56:45 -0500

    Added bg swap w/ space

--- Assets/camera.cs
+++ Assets/camera.cs
@@ -6,10 +6,28 @@ public class camera : MonoBehaviour
 {
     //Define a light blue color
     Color lightBlue = new Color(0.5f, 0.8f, 1f);
+    //define a darke blue color
+    Color darkBlue = new Color(0.2f, 0.4f, 0.6f);
 
     void Start()
     {
         GetComponent<Camera>().backgroundColor = lightBlue;
+    }
+    
+    void Update()
+    {
+        //if the player presses the spacebar, swap  the background color
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GetComponent<Camera>().backgroundColor == lightBlue)
+            {
+                GetComponent<Camera>().backgroundColor = darkBlue;
+            }
+            else
+            {
+                GetComponent<Camera>().backgroundColor = lightBlue;
+            }
+        }
     }
 
 }
